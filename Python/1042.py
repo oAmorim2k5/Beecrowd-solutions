706230634c8523d6d95acd4a83ea2d815bdfe61d
oAmorim2k5 <victorhuski@gmail.com>
2024-07-10 14:06:11 -0300

    1042 - python

--- Python/1042.py
+++ Python/1042.py
@@ -1,2 +1,24 @@
-a, b, c = int(input).split(" ")
+entry = input().split(" ")
 
+numeric_list = list(map(int, entry))
+list_sorted = sorted(numeric_list)
+
+for i, elements in enumerate(list_sorted):
+    print(elements)
+print("")
+for i, elements in enumerate(numeric_list):
+    print(elements)
+
+"""
+x,y,z = list(map(int, input().split(" ")))
+entry = [x, y, z]
+entry.sort()
+
+print(entry[0])
+print(entry[1])
+print(entry[2])
+print("")
+print(x)
+print(y)
+print(z)
+"""
